64d84b3cd1d46a14f7017e2bc9aad39b87e3ed05
Dave Slager <slager@users.noreply.github.com>
2023-12-30 21:50:31 -0800

    use devtools::load_all() now

--- ebird_filter_compare.R
+++ ebird_filter_compare.R
@@ -1,16 +1,6 @@
-####
-
-
 ## Specify portions of filter names to be deleted
 filter_prefix <- 'Wisconsin--\\d{2}\\.'
 filter_suffix <- ''
-
-
-####
-
-
-## Load the required functions
-source(file.path('R', 'functions.R'))
 
 ## Get list of filter filenames
 get_filenames() -> files
@@ -18,18 +8,11 @@ get_filenames() -> files
 ## Export CSV list of regions for manual ordering
 export_region_list_for_ordering(files, filter_prefix, filter_suffix) -> regions
 
-
-####
-
-
 ## Import manually ordered list of regions
 import_ordered_region_list() -> ordered_regions
 
 ## Check ordered_regions vs. regions for consistency
 check_regions(regions, ordered_regions)
-
-
-####
 
 ## Compile the taxa from the list of filters
 compile_taxonomy(files, regions) -> species
